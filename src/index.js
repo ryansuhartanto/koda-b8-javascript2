3fb74d04e822fe8e6e023490e2e0816e9ede14bd
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-05-20 14:07:38 +0700

    feat; implement min, max, avg

---
+++ src/index.js
@@ -0,0 +1,35 @@
+function main(data) {
+	if (!Array.isArray(data) || data.length === 0) {
+		throw new Error("data harus berupa array dan tidak boleh kosong");
+	}
+
+	let min = data[0];
+	let max = data[0];
+	let total = data[0];
+
+	for (let i = 1; i < data.length; i++) {
+		const check = data[i];
+
+		if (typeof check !== "number") {
+			throw new Error("data harus berisi number");
+		}
+
+		if (check < min) {
+			min = check;
+		}
+		if (check > max) {
+			max = check;
+		}
+
+		total += check;
+	}
+
+	const avg = total / data.length;
+
+	console.log(`Min: ${min}`);
+	console.log(`Max: ${max}`);
+	console.log(`Avg: ${avg}`);
+}
+
+// main([2, 4, 6, 8]);
+main([0, 1, "s"]);
