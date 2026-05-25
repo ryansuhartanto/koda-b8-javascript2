7bb74734db05bdb8e2d5e96fc544bf73ca0bb680
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-05-25 10:33:41 +0700

    test: cover everything

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,7 +1,7 @@
 import assert from "node:assert/strict";
-import { describe, it, mock } from "node:test";
+import { describe, it } from "node:test";
 
-import { calculate } from "./utils";
+import { calculate } from "./utils.js";
 
 describe("calculate", () => {
 	it("throws when data is undefined", () => {
@@ -15,5 +15,23 @@ describe("calculate", () => {
 	});
 	it("throws when data contains type other than number", () => {
 		assert.throws(() => calculate([1, 2, "3"]));
+	});
+
+	it("returns an array with length of 3", () => {
+		const result = calculate([0]);
+
+		assert.ok(Array.isArray(result));
+		assert.equal(result.length, 3);
+	});
+	it("returns [min, max, avg]", () => {
+		const param = [2, 1, 3];
+		const expectedMin = 1;
+		const expectedMax = 3;
+		const expectedAvg = 2;
+		const [min, max, avg] = calculate(param);
+
+		assert.equal(min, expectedMin);
+		assert.equal(max, expectedMax);
+		assert.equal(avg, expectedAvg);
 	});
 });
